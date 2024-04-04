0f6436aa8b59577509ba8cd7ab475abf512399bb
Bhakti Patil <166040320+bhaktipatil7@users.noreply.github.com>
2024-04-04 21:06:12 +0530

    Add files via upload

---
+++ python/prog8.py
@@ -0,0 +1,19 @@
+string = 'amaama'
+half = int(len(string) / 2)
+ 
+ 
+first_str = string[:half]
+second_str = string[half:]
+ 
+ 
+# symmetric
+if first_str == second_str:
+    print(string, 'string is symmetrical')
+else:
+    print(string, 'string is not symmetrical')
+ 
+# palindrome
+if first_str == second_str[::-1]:  # ''.join(reversed(second_str)) [slower]
+    print(string, 'string is palindrome')
+else:
+    print(string, 'string is not palindrome')
